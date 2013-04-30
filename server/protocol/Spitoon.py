6fa83300546e8221464985a62a1005dfb3c15e6d
dev_marlon <marlon@rightshift.biz>
2013-04-30 19:09:56 +0200

    Spitoon twisted protocol added. New tangent here...

---
+++ server/protocol/Spitoon.py
@@ -0,0 +1,21 @@
+from twisted.internet.protocol import Protocol
+
+class Spitoon(Protocol):
+    
+    def __init__ (self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        print "new connection in"
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        self.transport.write("you're spitting as number %d" % (self.factory.numProtocols, ))
+
+    def connectionLost(self, reason):
+        print "connection dropped"
+        self.factory.numProtocols = self.factory.numProtocols - 1
+
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+
+
