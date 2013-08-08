169ed2e0c22692123feaaf33e785375e875a80d7
dev_marlon <marlon@rightshift.biz>
2013-08-08 10:08:18 +0200

    Licenser.sh applied license headers into the server/*.py files.

--- server/core/license.py
+++ server/core/license.py
@@ -1,3 +1,22 @@
+##
+#    Copyright 2013 Marlon van der Linde
+#
+#    This file is part of BurningToast
+#
+#    BurningToast is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BurningToast is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BurningToast.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 fullGPL = """GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
